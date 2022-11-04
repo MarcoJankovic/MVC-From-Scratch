b61f3009e2f83285cccc23187ff0669ff5f8aa84
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-04 11:28:08 +0100

    Add project files.

---
+++ MVC-From-Scratch/Controllers/DoctorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class DoctorController : Controller
+    {
+        [HttpPost]
+        public IActionResult FeverCheck(double temp)
+        {
+            if(temp != 0)
+            {
+                ViewBag.Msg = Models.Doctor.Diagnose(temp);
+                return View();
+            }
+            else
+            {
+                ViewBag.Msg = "Temperature not found!.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-From-Scratch/Models/Doctor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace MVC_From_Scratch.Models
+{
+    public class Doctor
+    {
+     
+        public static string Diagnose(double temp)
+        {
+            if (temp == 37)
+            {
+                return "You are perfectly fine!";
+            } 
+            else if (temp <= 36 && temp >= 37)
+            {
+                return " You should get some rest!";
+            }
+            else if (temp < 30)
+            {
+                return "You are dead! :D";
+            }
+
+            return "No Valid Temperature";
+        }
+       
+    }
+
+}
