a547f9aa3bfecdb57eaee9f9caaf1e9335257fbb
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-08 16:19:29 +0100

    Newest

---
+++ MVC-From-Scratch/Controllers/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_From_Scratch.Models;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpPost]
+        public IActionResult GameGuess(int guess)
+        {
+            if (!(string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd")) || guess <= 0) || guess >= 100)
+            {
+                int storeRnd = (int)HttpContext.Session.GetInt32("intRnd");
+                string respons = GuessGameNr.MatchGuess(Convert.ToInt32(guess), storeRnd);
+
+                ViewBag.Msg = respons;
+            }
+            else
+            {
+                ViewBag.Msg = "Hutta in ett nummber";
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GameGuess()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("intRnd")))
+            {
+                int getRnd = GuessGameNr.GetRandom(100);
+                HttpContext.Session.SetInt32("intRnd", getRnd);
+                ViewBag.Rnd = getRnd;
+            }
+            else
+            {
+                ViewBag.Rnd = HttpContext.Session.GetString("intRnd");
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+
+        public IActionResult BortBort()
+        {
+            int getRnd = GuessGameNr.GetRandom(100);
+            HttpContext.Session.SetInt32("intRnd", getRnd);
+            return RedirectToAction(nameof(GameGuess));
+        }
+    }
+}
---
+++ MVC-From-Scratch/Models/GuessGameNr.cs
@@ -0,0 +1,29 @@
+﻿namespace MVC_From_Scratch.Models
+{
+    public class GuessGameNr
+    {
+        public static string MatchGuess(int guess, int rnd)
+        {
+            if (guess < rnd)
+            {
+                return "Higher... stupid!!";
+            }
+            else if (guess > rnd)
+            {
+                return "Lower OMG how low do you have to go ?";
+            }
+            else
+            {
+                return "Yo... you cheating!!, Gz..";
+            }
+        }
+
+        public static int GetRandom(int max)
+        {
+            Random random = new Random();
+            return random.Next(max);
+
+        }
+    }
+
+}
